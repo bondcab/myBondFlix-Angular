9ed059a47b764678a14b06935afd2d4c0985c382
bondcab <136003551+bondcab@users.noreply.github.com>
2023-12-16 08:42:17 +0000

    Update fetch-api-data.service.ts

--- src/app/fetch-api-data.service.ts
+++ src/app/fetch-api-data.service.ts
@@ -43,6 +43,30 @@ export class UserRegistrationService {
 export class FetchApiDataService {
   constructor(private http: HttpClient) {}
 
+  // User login endpoint
+  userLogin(userDetails: any): Observable<any> {
+    return this.http
+      .post(apiUrl + 'login?', new URLSearchParams(userDetails), {})
+      .pipe(catchError(this.handleError));
+  }
+
+  // Unregister user endpoint
+  userDelete(userDetails: any): Observable<any> {
+    const user = JSON.parse(localStorage.getItem('user') || '{}');
+    return this.http
+      .delete(apiUrl + user.userName, {})
+      .pipe(catchError(this.handleError));
+  }
+
+  // Update user information endpoint
+  userUpdate(userDetails: any): Observable<any> {
+    const user = localStorage.getItem('user');
+    return this.http
+      .put(apiUrl + 'users/' + user, {})
+      .pipe(catchError(this.handleError));
+  }
+
+  // Get data for all movies endpoint
   getAllMovies(): Observable<any> {
     const token = localStorage.getItem('token');
     return this.http
@@ -53,8 +77,87 @@ export class FetchApiDataService {
       })
       .pipe(map(this.extractResponseData), catchError(this.handleError));
   }
+
+  // Adding favourite movie endpoint
+  addFavourite(movieID: string): Observable<any> {
+    const token = localStorage.getItem('token');
+    const user = JSON.parse(localStorage.getItem('user') || '{}');
+    user.FavoriteMovies.push(movieID);
+    return this.http
+      .post(apiUrl + 'users/' + user + 'movies' + movieID, {
+        headers: new HttpHeaders({
+          Authorization: 'Bearer ' + token,
+        }),
+      })
+      .pipe(map(this.extractResponseData), catchError(this.handleError));
+  }
+
+  // Remove favourite movie point
+  removeFavourite(movieID: string): Observable<any> {
+    const token = localStorage.getItem('token');
+    const user = JSON.parse(localStorage.getItem('user') || '{}');
+    const index = user.FavoriteMovies.indexOf(movieID);
+    if (index >= 0) {
+      user.FavoriteMovies.splice(index, 1);
+    }
+    return this.http
+      .delete(apiUrl + 'users/' + user + 'movies/' + movieID, {
+        headers: new HttpHeaders({
+          Authorization: 'Bearer ' + token,
+        }),
+      })
+      .pipe(map(this.extractResponseData), catchError(this.handleError));
+  }
+
+  // Get data on one movie endpoint
+  getMovie(title: string): Observable<any> {
+    const token = localStorage.getItem('token');
+    return this.http
+      .get(apiUrl + 'movies/' + title, {
+        headers: new HttpHeaders({
+          Authorization: 'Bearer ' + token,
+        }),
+      })
+      .pipe(map(this.extractResponseData), catchError(this.handleError));
+  }
+
+  // Get data on one director endpoint
+  getDirector(directorName: string): Observable<any> {
+    const token = localStorage.getItem('token');
+    return this.http
+      .get(apiUrl + 'movies/' + directorName, {
+        headers: new HttpHeaders({
+          Authorization: 'Bearer ' + token,
+        }),
+      })
+      .pipe(map(this.extractResponseData), catchError(this.handleError));
+  }
+
+  // Get data on actor endpoint
+  getActor(actorName: string): Observable<any> {
+    const token = localStorage.getItem('token');
+    return this.http
+      .get(apiUrl + 'movies/' + actorName, {
+        headers: new HttpHeaders({
+          Authorization: 'Bearer ' + token,
+        }),
+      })
+      .pipe(map(this.extractResponseData), catchError(this.handleError));
+  }
+
+  //
+  private handleError(error: HttpErrorResponse): any {
+    if (error.error instanceof ErrorEvent) {
+      console.error('Some error occurred:', error.error.message);
+    } else {
+      console.error(
+        `Error Status code ${error.status}, ` + `Error body is: ${error.error}`
+      );
+    }
+    return throwError('Something bad happened; please try again later.');
+  }
   // Non-typed response extraction
-  private extractResponseData(res: Response): any {
+  private extractResponseData(res: any): any {
     const body = res;
     return body || {};
   }
